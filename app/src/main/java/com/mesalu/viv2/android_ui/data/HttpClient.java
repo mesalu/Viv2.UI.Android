70941d2fdbe718bc7845f7ad2075208a510a8da2
mesalu <samuel@mesalu.com>
2021-03-09 21:54:55 -0800

    Initial project creation & HTTPS support.
    Use Volley for making requests in background threads
    Setting this up for development ended up requiring far more app-side work than I would like. There's a couple of security holes that had to be drilled into the HTTPS stack in order to operate locally with a self signed certificate.(Deploying the cert to the device and using it as a KeyStore, and verifying any hostname that matches the internal IP address I typically run dev builds of RestAPI from.)

---
+++ app/src/main/java/com/mesalu/viv2/android_ui/data/HttpClient.java
@@ -0,0 +1,110 @@
+package com.mesalu.viv2.android_ui.data;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.Volley;
+import com.mesalu.viv2.android_ui.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyStore;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+public class HttpClient {
+    private static class HostNamePermissiveHurlStack extends HurlStack {
+        public HostNamePermissiveHurlStack(UrlRewriter rewriter, SSLSocketFactory sslSocketFactory) {
+            super(rewriter, sslSocketFactory);
+        }
+
+        @Override
+        protected HttpURLConnection createConnection(URL url) throws IOException {
+            HttpsURLConnection conn = (HttpsURLConnection) super.createConnection(url);
+            conn.setHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    if (hostname.equals("192.168.0.10")) return true;
+                    return HttpsURLConnection.getDefaultHostnameVerifier().verify(hostname, session);
+                }
+            });
+            return conn;
+        }
+    }
+
+    private static HttpClient instance;
+    private RequestQueue requestQueue;
+    private static Context ctx;
+
+    // For authorizing my self-signed-cert in development:
+    private static char[] KEYSTORE_PASSWORD = "demopass".toCharArray();
+
+    private HttpClient(Context context) {
+        ctx = context.getApplicationContext();
+
+        // WARN: this is a stop gap while development occurs. Before long this should
+        //       be removed in favor of adding a ssl cert to dev devices.
+
+
+        // NOTE: example has a semi-singleton setup for this member, where
+        //      a getter method is used to check for existence and instantiate if
+        //      missing, much like a singleton instance fetcher.
+        //      It would make sense if contexts can persist through changes that would
+        //      otherwise reset state (thus making the requestQueue member null).
+        //      I am... skeptical.. this is true though, so we'll just do a one-and-done
+        //      instantiation. If there are errors where this member is null where it should
+        //      not be, then taking an approach in-line with the example would be the thing
+        //      to do.
+        requestQueue = Volley.newRequestQueue(ctx, new HostNamePermissiveHurlStack(null, newSslSocketFactory()));
+    }
+
+    public static synchronized HttpClient getInstance(Context context) {
+        if (instance == null)
+            instance = new HttpClient(context);
+        return instance;
+    }
+
+    // Creates a socket factory configured to trust self-signed keys stored in res.raw.dev_keystore.
+    private SSLSocketFactory newSslSocketFactory() {
+        try {
+            // Get an instance of the Bouncy Castle KeyStore format
+            KeyStore trusted = KeyStore.getInstance("PKCS12");
+
+            // Get the raw resource, which contains the keystore with
+            // your trusted certificates (root and any intermediate certs)
+            InputStream in = ctx.getApplicationContext().getResources().openRawResource(R.raw.dev_keystore);
+            try {
+                // Initialize the keystore with the provided trusted certificates
+                // Provide the password of the keystore
+                trusted.load(in, KEYSTORE_PASSWORD);
+            } finally {
+                in.close();
+            }
+
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(trusted);
+
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, tmf.getTrustManagers(), null);
+
+            SSLSocketFactory sf = context.getSocketFactory();
+            return sf;
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public <T> Request<T> add(Request<T> r) {
+        return requestQueue.add(r);
+    }
+}
---
+++ app/src/main/java/com/mesalu/viv2/android_ui/ui/login/LoginActivity.java
@@ -0,0 +1,175 @@
+package com.mesalu.viv2.android_ui.ui.login;
+
+import android.app.Activity;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.mesalu.viv2.android_ui.R;
+import com.mesalu.viv2.android_ui.data.HttpClient;
+import com.mesalu.viv2.android_ui.ui.login.LoginViewModel;
+import com.mesalu.viv2.android_ui.ui.login.LoginViewModelFactory;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginViewModel loginViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+            @Override
+            public void onChanged(@Nullable LoginFormState loginFormState) {
+                if (loginFormState == null) {
+                    return;
+                }
+                loginButton.setEnabled(loginFormState.isDataValid());
+                if (loginFormState.getUsernameError() != null) {
+                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
+                }
+                if (loginFormState.getPasswordError() != null) {
+                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
+                }
+            }
+        });
+
+        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+            @Override
+            public void onChanged(@Nullable LoginResult loginResult) {
+                if (loginResult == null) {
+                    return;
+                }
+                loadingProgressBar.setVisibility(View.GONE);
+                if (loginResult.getError() != null) {
+                    showLoginFailed(loginResult.getError());
+                }
+                if (loginResult.getSuccess() != null) {
+                    updateUiWithUser(loginResult.getSuccess());
+                }
+                setResult(Activity.RESULT_OK);
+
+                //Complete and destroy login activity once successful
+                finish();
+            }
+        });
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ignore
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
+                        passwordEditText.getText().toString());
+            }
+        };
+        usernameEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    loginViewModel.login(usernameEditText.getText().toString(),
+                            passwordEditText.getText().toString());
+                }
+                return false;
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+
+                //loginViewModel.login(usernameEditText.getText().toString(),
+                //        passwordEditText.getText().toString());
+
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String url = "https://192.168.0.10:5001/api/login";
+
+                JSONObject loginObject = new JSONObject();
+                try {
+                    loginObject.put("username", username);
+                    loginObject.put("password", password);
+
+                    Log.d("LA", "Set up login object");
+                }
+                catch (JSONException e) {
+                    // no big deal?
+                    Log.d("LA", "Failed to set up login object");
+                }
+
+                HttpClient client = HttpClient.getInstance(LoginActivity.this);
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
+                        url,
+                        loginObject,
+                        response -> {
+                            Log.d("LA", "Login result: " + response.toString());
+                            loadingProgressBar.setVisibility(View.GONE);
+                        },
+                        error -> {
+                            // TODO: Handle error
+                            Log.d("LA", "Error when making request! " + error.toString());
+                            loadingProgressBar.setVisibility(View.GONE);
+                        });
+
+                client.add(request);
+                Log.d("LA", "added request to queue!");
+            }
+        });
+    }
+
+    private void updateUiWithUser(LoggedInUserView model) {
+        String welcome = getString(R.string.welcome) + model.getDisplayName();
+        // TODO : initiate successful logged in experience
+        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+    }
+
+    private void showLoginFailed(@StringRes Integer errorString) {
+        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    }
+}
